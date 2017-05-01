5c4cb95c8e04e4f7cb0f03fa8ec75032c2ba4efe
Kylart <garnier@et.esiea.fr>
2017-05-02 00:30:22 +0200

    Better handling of nyaa.se status

--- index.js
+++ index.js
@@ -8,30 +8,33 @@ const searchTerm = (term, number = null) => {
   return new Promise((resolve, reject) => {
     ping.sys.probe('nyaa.se', (isAlive) => {
       if (!isAlive) reject('[Nyaa]: Nyaa.se is down...')
-    })
 
-    req(`https://www.nyaa.se/?page=rss&cats=1_0&filter=0&term=${term.split(' ').join('+')}`, (err, resp) => {
-      if (err) throw reject(err)
+      else
+      {
+      	req(`https://www.nyaa.se/?page=rss&cats=1_0&filter=0&term=${term.split(' ').join('+')}`, (err, resp) => {
+	      if (err) throw reject(err)
 
-      const xml = resp.body
+	      const xml = resp.body
 
-      parseString(xml, (err, result) => {
-        if (err) reject(err)
+	      parseString(xml, (err, result) => {
+	        if (err) reject(err)
 
-        const t2 = new Date()
-        const duration = (t2 - t1) / 1000
+	        const t2 = new Date()
+	        const duration = (t2 - t1) / 1000
 
-        const torrents = result.rss.channel[0].item
+	        const torrents = result.rss.channel[0].item
 
-        if (!number) number = torrents.length
+	        if (!number) number = torrents.length
 
-        const statMsg = `[Nyaa]: ${number} torrents gathered in ${duration}s.`
+	        const statMsg = `[Nyaa]: ${number} torrents gathered in ${duration}s.`
 
-        resolve({
-          items: torrents.slice(0, number),
-          statMsg: statMsg
-        })
-      })
+	        resolve({
+	          items: torrents.slice(0, number),
+	          statMsg: statMsg
+	        })
+	      })
+	    })
+      }
     })
   })
 }
