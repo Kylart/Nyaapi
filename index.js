c3e8eae1ce15694c48641e3bf5e90f8619ffaf10
Kylart <garnier@et.esiea.fr>
2017-05-20 15:30:32 +0200

    Removed ping

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const ping = require('ping')
 const parseString = require('xml2js').parseString
 const axios = require('axios')
 
@@ -7,48 +6,40 @@ const NYAA_SI_URI = 'https://nyaa.si/?page=rss&c=1_0&f=0&q='
 
 const searchPantsu = (term, n = null) => {
   return new Promise((resolve, reject) => {
-    ping.sys.probe('nyaa.pantsu.cat', (isAlive) => {
-      if (!isAlive) reject(new Error('Nyaa.pantsu.cat is down.'))
+    axios.get(`${NYAA_PANTSU_URI}${term.split(' ').join('+')}`).then(({data}) => {
+      parseString(data, (err, result) => {
+        if (err) reject(err)
 
-      axios.get(`${NYAA_PANTSU_URI}${term.split(' ').join('+')}`).then(({data}) => {
-        parseString(data, (err, result) => {
-          if (err) reject(err)
+        const results = result.rss.channel[0].item
 
-          const results = result.rss.channel[0].item
-
-          results === undefined
-            ? reject(new Error('[Nyaa]: No result found...'))
-            : n
-            ? resolve(results.slice(0, n))
-            : resolve(results)
-        })
-      }).catch((err) => {
-        reject(err)
+        results === undefined
+          ? reject(new Error('[Nyaa]: No result found...'))
+          : n
+          ? resolve(results.slice(0, n))
+          : resolve(results)
       })
+    }).catch((err) => {
+      reject(err)
     })
   })
 }
 
 const searchSi = (term, n = null) => {
   return new Promise((resolve, reject) => {
-    ping.sys.probe('nyaa.si', (isAlive) => {
-      if (!isAlive) reject(new Error('Nyaa.si is down.'))
+    axios.get(`${NYAA_SI_URI}${term.split(' ').join('+')}`).then(({data}) => {
+      parseString(data, (err, result) => {
+        if (err) reject(err)
 
-      axios.get(`${NYAA_SI_URI}${term.split(' ').join('+')}`).then(({data}) => {
-        parseString(data, (err, result) => {
-          if (err) reject(err)
+        const results = result.rss.channel[0].item
 
-          const results = result.rss.channel[0].item
-
-          results === undefined
-            ? reject(new Error('[Nyaa]: No result found...'))
-            : n
-            ? resolve(results.slice(0, n))
-            : resolve(results)
-        })
-      }).catch((err) => {
-        reject(err)
+        results === undefined
+          ? reject(new Error('[Nyaa]: No result found...'))
+          : n
+          ? resolve(results.slice(0, n))
+          : resolve(results)
       })
+    }).catch((err) => {
+      reject(err)
     })
   })
 }
