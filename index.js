b2432525f5567c966f8154947c78ac5112495f81
Kylart <garnier@et.esiea.fr>
2017-05-20 15:00:54 +0200

    Implmented nyaa.si rss + tests

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const parseString = require('xml2js').parseString
 const axios = require('axios')
 
 const NYAA_PANTSU_URI = 'https://nyaa.pantsu.cat/feed?c=_&s=0&max=99999&userID=0&q='
+const NYAA_SI_URI = 'https://nyaa.si/?page=rss&c=1_0&f=0&q='
 
 const searchPantsu = (term, n = null) => {
   return new Promise((resolve, reject) => {
     ping.sys.probe('nyaa.pantsu.cat', (isAlive) => {
-      if (!isAlive) reject('Nyaa.pantsu.cat is down')
+      if (!isAlive) reject('Nyaa.pantsu.cat is down.')
 
       axios.get(`${NYAA_PANTSU_URI}${term.split(' ').join('+')}`).then(({data}) => {
         parseString(data, (err, result) => {
           if (err) reject(err)
 
-          const results = result.feed.entry
+          const results = result.rss.channel[0].item
 
           results === undefined
             ? reject('[Nyaa]: No result found...')
@@ -28,6 +29,32 @@ const searchPantsu = (term, n = null) => {
   })
 }
 
+const searchSi = (term, n = null) => {
+  return new Promise((resolve, reject) => {
+    ping.sys.probe('nyaa.si', (isAlive) => {
+      if (!isAlive) reject('Nyaa.si is down.')
+
+      axios.get(`${NYAA_SI_URI}${term.split(' ').join('+')}`).then(({data}) => {
+        parseString(data, (err, result) => {
+          if (err) reject(err)
+
+          const results = result.rss.channel[0].item
+
+          results === undefined
+            ? reject('[Nyaa]: No result found...')
+            : n
+            ? resolve(results.slice(0, n))
+            : resolve(results)
+        })
+
+      }).catch((err) => {
+        reject(err)
+      })
+    })
+  })
+}
+
 module.exports = {
-  searchPantsu
+  searchPantsu,
+  searchSi
 }
--- test/main.js
+++ test/main.js
@@ -1,35 +1,27 @@
 const colors = require('colors')
-const ping = require('ping')
+const axios = require('axios')
 const expect = require('chai').expect
 const path = require('path')
 const main = require(path.join(__dirname, '..', 'index.js'))
 
-const fansub = 'HorribleSubs'
+const fansub = '[HorribleSubs]'
 const quality = '720p'
 const anime = 'Youjo senki'
 const number = 18
 
 describe('Nyaa.pantsu.cat', () => {
-  before(function () {
-    console.log('Checking if Nyaa.pantsu.cat is up.'.yellow)
-    ping.sys.probe('nyaa.pantsu.cat', (isAlive) => {
-      if (!isAlive) console.log('Nyaa.pantsu.cat is down... Skipping.'.red)
-      else  console.log('Everything ok, processing to tests.'.green)
-    })
-  })
-
   describe(`"${fansub} ${quality} " results`, () => {
     it('should return 18 results', function () {
-      this.timeout(1500)
+      this.timeout(5000)
 
-      return main.searchPantsu(`[${fansub}] ${quality}`, number).then((data) => {
+      return main.searchPantsu(`${fansub} ${quality}`, number).then((data) => {
         expect(data.length).to.equal(18)
       })
     })
   })
   describe(`"${fansub} ${quality} ${anime}" results`, () => {
     it('should return some results', function () {
-      this.timeout(1500)
+      this.timeout(5000)
 
       return main.searchPantsu(`${fansub} ${quality}`, number).then((data) => {
         expect(data.length).to.be.above(1)
@@ -41,6 +33,36 @@ describe('Nyaa.pantsu.cat', () => {
       this.timeout(1500)
 
       return main.searchPantsu('[HorribleSabs] 570p', number).catch((err) => {
+        expect(err).to.equal('[Nyaa]: No result found...')
+      })
+    })
+  })
+})
+
+describe('Nyaa.si', () => {
+  describe(`"${fansub} ${quality} " results`, () => {
+    it('should return 18 results', function () {
+      this.timeout(1500)
+
+      return main.searchSi(`${fansub} ${quality}`, number).then((data) => {
+        expect(data.length).to.equal(18)
+      })
+    })
+  })
+  describe(`"${fansub} ${quality} ${anime}" results`, () => {
+    it('should return some results', function () {
+      this.timeout(1500)
+
+      return main.searchSi(`${fansub} ${quality}`, number).then((data) => {
+        expect(data.length).to.be.above(1)
+      })
+    })
+  })
+  describe('Wrong research: [HorribleSabs] 570p', () => {
+    it('should not have any result', function () {
+      this.timeout(1500)
+
+      return main.searchSi('[HorribleSabs] 570p', number).catch((err) => {
         expect(err).to.equal('[Nyaa]: No result found...')
       })
     })
