a7e376f907084ee315986a0b5de42aa8218cd507
Kylart <Kylart.dev@gmail.com>
2017-10-18 19:01:19 +0200

    Upload ready and working thanks to request

--- src/pantsu/index.js
+++ src/pantsu/index.js
@@ -1,7 +1,15 @@
 const search = require('./search.js')
 const info = require('./info.js')
+const upload = require('./upload.js')
+const update = require('./update.js')
+const login = require('./login')
+const checkUser = require('./checkUser.js')
 
 module.exports = {
   ...search,
-  ...info
+  ...info,
+  ...upload,
+  ...update,
+  ...login,
+  ...checkUser
 }
---
+++ src/pantsu/upload.js
@@ -0,0 +1,47 @@
+/**
+ * Resources explaining the choices
+ *
+ * https://github.com/gin-gonic/gin/issues/931
+ * https://github.com/axios/axios/issues/623
+ */
+
+const fs = require('fs')
+const req = require('request-promise')
+const _ = require('lodash')
+
+const URI = require('./url.json').url
+
+/**
+ * Allows the uploading of torrent file to nyaa.pantsu.cat
+ *
+ * @param {Object} opts Options.
+ * @param {boolean} verbose Whether or not Nyaapi should give the upload progress.
+ *
+ * @returns {promise}
+ */
+
+const upload = (opts = {}, verbose = false) => {
+  return new Promise((resolve, reject) => {
+    if ((!opts.magnet && !opts.torrent) || !opts.token || !opts.username) {
+      reject(new Error('[Nyaapi]: No file/torrent, token or username were given.'))
+    }
+
+    if (opts.torrent) {
+      opts.torrent = fs.createReadStream(opts.torrent)
+    }
+
+    req.post({
+      url: `${URI}upload`,
+      headers: {
+        'Authorization': opts.token
+      },
+      formData: _.omit(opts, 'token')
+    })
+      .then((data) => resolve(data))
+      .catch((err) => reject(err))
+  })
+}
+
+module.exports = {
+  upload
+}
