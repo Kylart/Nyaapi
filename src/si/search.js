d172d87ca689f7b6d1fbdd752ea2caf6d5c8b29f
Kylart <Kylart.dev@gmail.com>
2017-10-19 17:43:16 +0200

    si's search is now fully fonctioning Added some adjustment to previous search methods

--- src/si/search.js
+++ src/si/search.js
@@ -4,8 +4,27 @@ const _ = require('lodash')
 
 const URI = require('./url.json').url
 
-const _searchPage = (term, p, opts = {}) => {
+/**
+ * Allows to scrap only one specific page of a research
+ *
+ * @param {string} term Keywords describing the research.
+ * @param {number} p The page you want to look for.
+ * @param {object} opts Research options as described on the official documentation (optional)
+ *
+ * @returns {promise}
+ */
+
+const searchPage = (term = null, p, opts = {}) => {
   return new Promise((resolve, reject) => {
+    if (typeof term === 'object') {
+      opts = term
+      term = opts.term
+      p = p || opts.p
+    }
+
+    if (!term) reject(new Error('[Nyaapi]: No term was given on search demand.'))
+    if (!p) reject(new Error('[Nyaapi]: No page number was given on search page demand.'))
+
     axios.get(URI, {
       params: {
         f: opts.filter || 0,
@@ -79,23 +98,18 @@ const searchAll = (term = null, opts = {}) => {
     }
 
     if (typeof term === 'object') {
-      term = term.term
+      opts = term
+      term = opts.term
     }
 
-    try {
-      tmpData = await _searchPage(term, page, opts)
-    } catch (e) {
-      reject(e)
-    }
-
-    while (tmpData.length && page < 15) {
+    while (page <= 15) {
       // We stop at page === 15 because nyaa.si offers a maximum of 1000 results
       // which means 14 pages of 75 results with the last one containing only 25.
       try {
         results = _.concat(results, tmpData)
 
+        tmpData = await searchPage(term, page, opts)
         ++page
-        tmpData = await _searchPage(term, page, opts)
       } catch (e) {
         reject(e)
       }
@@ -116,9 +130,35 @@ const searchAll = (term = null, opts = {}) => {
  * @returns {promise}
  */
 
-const search = (term, n = null, opts = {}) => {
-  return new Promise((resolve, reject) => {
+const search = (term = null, n = null, opts = {}) => {
+  return new Promise(async (resolve, reject) => {
+    if (typeof term === 'object') {
+      opts = term
+      term = opts.term
+      n = n || opts.n
+    }
 
+    // If there is no n, then the user's asking for all the results, right?
+    if (!n) {
+      resolve(searchAll(term, opts))
+    } else {
+      let results = []
+      let tmpData = []
+      let page = 1
+      const maxPage = Math.ceil(n / 75)
+
+      while (page <= maxPage) {
+        try {
+          tmpData = await searchPage(term, page, opts)
+          results = _.concat(results, tmpData)
+          ++page
+        } catch (e) {
+          reject(e)
+        }
+      }
+
+      resolve(results.slice(0, n))
+    }
   })
 }
 
@@ -141,5 +181,8 @@ const searchByUser = (user, term, n = null, opts = {}) => {
 }
 
 module.exports = {
-  search
+  search,
+  searchAll,
+  searchPage,
+  searchByUser
 }
